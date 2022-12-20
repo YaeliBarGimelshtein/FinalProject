a70b9ca0e882d2e1d4fa69c98aae46b90b4360a2
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2022-12-20 08:51:34 +0200

    created constants and updated sceneLoaderToCastleA

---
+++ Final Project/Assets/Scripts/Constants.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants 
+{
+    //Scenes Indexes
+    public const int MedievalEnvironmentScene = 0;
+    public const int CastleAScene = 1;
+    public const int CastleBScene = 2;
+
+    //Transition Time Between Scenes
+    public const float TransitionTimeBetweenScenes = 1f;
+
+    //Save Position On Exit Medieval Environment Scene
+    public const int AdditionalOnXAxesLeavingMedievalEnviromentScene = 12;
+    public const int AdditionalOnYAxesLeavingMedievalEnviromentScene = 3;
+}
--- Final Project/Assets/Scripts/SceneLoaderToCastleA.cs
+++ Final Project/Assets/Scripts/SceneLoaderToCastleA.cs
@@ -6,21 +6,21 @@ using UnityEngine.SceneManagement;
 public class SceneLoaderToCastleA : MonoBehaviour
 {
     public Animator transition;
-    public float transitionTime = 1f;
     public GameObject player;
-    int nextScene = -1;
-
+    
     private void OnTriggerEnter(Collider other)
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = -1;
+
         switch (currentScene)
         {
-            case 0:
-                nextScene = 1;
+            case Constants.MedievalEnvironmentScene:
+                nextScene = Constants.CastleAScene;
                 SavePositionOnExitMainScene();
                 break;
-            case 1:
-                nextScene = 0;
+            case Constants.CastleAScene:
+                nextScene = Constants.MedievalEnvironmentScene;
                 break;
         }
         StartCoroutine(LoadLevel(nextScene));
@@ -32,7 +32,7 @@ public class SceneLoaderToCastleA : MonoBehaviour
         transition.SetTrigger("Start");
 
         //Wait
-        yield return new WaitForSeconds(transitionTime);
+        yield return new WaitForSeconds(Constants.TransitionTimeBetweenScenes);
 
         //Load Scene
         SceneManager.LoadScene(levelIndex);
@@ -41,9 +41,9 @@ public class SceneLoaderToCastleA : MonoBehaviour
     private void SavePositionOnExitMainScene()
     {
         var location = gameObject.transform.position;
-        location.x += 12;
-        location.y -= 3;
+        location.x += Constants.AdditionalOnXAxesLeavingMedievalEnviromentScene;
+        location.y -= Constants.AdditionalOnYAxesLeavingMedievalEnviromentScene;
         GlobalPlayerManagement.instance.playerLocationOnExitCastleA = location;
-        GlobalPlayerManagement.instance.lastScene = 1;
+        GlobalPlayerManagement.instance.lastScene = Constants.CastleAScene;
     }
 }
