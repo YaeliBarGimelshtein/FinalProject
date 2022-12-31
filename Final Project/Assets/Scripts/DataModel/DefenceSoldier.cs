04798e468ca4adabf49f504f10c20aff4bf51903
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2022-12-31 15:21:06 +0200

    added navigation and agent mesh to the paladin

--- Final Project/Assets/Scripts/DataModel/DefenceSoldier.cs
+++ Final Project/Assets/Scripts/DataModel/DefenceSoldier.cs
@@ -1,18 +1,29 @@
+using Assets.Scripts.Contract.NPC;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class DefenceSoldier : Soldier
+public class DefenceSoldier : Soldier, IDefenceSoldierBehaviour
 {
+    private Animator animator;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Patrol()
+    {
+        throw new System.NotImplementedException();
     }
 }
