52c0fb37801bc472868540ff5cc333fac7eb89ca
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-01-17 18:14:19 +0200

    fixed lighting issue

--- Final Project/Assets/Scripts/BusinessLogic/Components/MenuManager/MainMenu.cs
+++ Final Project/Assets/Scripts/BusinessLogic/Components/MenuManager/MainMenu.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private Animator transition;
+
+    void Start()
+    {
+        transition = GetComponent<Animator>();
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        StartCoroutine(LoadLevel(nextScene));
     }
-    
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play Animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(Constants.TransitionTimeBetweenScenes);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
