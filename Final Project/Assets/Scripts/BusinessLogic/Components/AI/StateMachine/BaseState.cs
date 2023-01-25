cf53f6cb23abbccdd0a733e830efaf95bea42df5
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-01-25 13:28:03 +0200

    finished state machine skeleton

--- Final Project/Assets/Scripts/BusinessLogic/Components/AI/StateMachine/BaseState.cs
+++ Final Project/Assets/Scripts/BusinessLogic/Components/AI/StateMachine/BaseState.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Class <c>BaseState</c> is a blueprint or pattern for all the actual states that are going to inherit from this class
+/// Class BaseState is a blueprint or pattern for all the actual states that are going to inherit from this class.
+/// It has 3 main methods - Enter, Update and Exit.
 /// </summary>
 public class BaseState
 {
+    public string name;
+    protected StateMachine stateMachine;
+
+    public BaseState(string stateName, StateMachine machine)
+    {
+        name = stateName;
+        stateMachine = machine;
+    }
+
     /// <summary>
     /// perform all initializations needed when transition to the state
     /// </summary>
     public virtual void Enter() { }
 
     /// <summary>
-    /// <para>perform the logic of the state and happens continuously while entity in this state. </para>
-    /// <remarks>
-    /// in unity life cycle there are a several updates in a frame. <c>Update</c> called in the beginning of a frame,
-    /// <c>FixedUpdate</c> and <c>LateUpdate</c> that are called later on in a frame.
-    /// There are a way of separating logic so that everything runs smoothly and in the proper order.
-    /// To mimic this, we will have 2 types of update methods.
-    /// </remarks>
+    /// perform the logic of the state and happens continuously while entity in this state. 
     /// </summary>
     public virtual void UpdateLogic() { }
 
     /// <summary>
-    /// <para>perform the physics of the state and happens continuously while entity in this state. </para>
-    
+    /// perform the physics of the state and happens continuously while entity in this state.
     /// </summary>
     public virtual void UpdatePhysics() { }
 
