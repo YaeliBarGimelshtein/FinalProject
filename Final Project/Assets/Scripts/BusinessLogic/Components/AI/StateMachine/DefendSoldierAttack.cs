9cba2169007bea7e8e200d262f7bd4443b0daef6
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-02-25 19:19:51 +0200

    Added dying state

--- Final Project/Assets/Scripts/BusinessLogic/Components/AI/StateMachine/DefendSoldierAttack.cs
+++ Final Project/Assets/Scripts/BusinessLogic/Components/AI/StateMachine/DefendSoldierAttack.cs
@@ -24,6 +24,10 @@ public class DefendSoldierAttack : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if(soldierData.Health == 0)
+        {
+            animator.SetTrigger("Die");
+        }
         if(enemy.isAttacking)
         {
             animator.SetTrigger("Defend");
