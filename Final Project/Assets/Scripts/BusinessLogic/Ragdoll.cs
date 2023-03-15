a0a5f89f0f7684c151ff57eadea282ed752335cd
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-03-15 15:54:29 +0200

    started a big change regarding the characters

---
+++ Final Project/Assets/Scripts/BusinessLogic/Ragdoll.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    Rigidbody[] rigitBodies;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigitBodies = GetComponentsInChildren<Rigidbody>();
+        animator = GetComponent<Animator>();
+        DeactivateRagdoll();
+    }
+
+    public void DeactivateRagdoll()
+    {
+        foreach(var rigidBody in rigitBodies)
+        {
+            rigidBody.isKinematic = true;
+        }
+        animator.enabled = true;
+    }
+
+    public void ActivateRagdoll()
+    {
+        foreach (var rigidBody in rigitBodies)
+        {
+            rigidBody.isKinematic = false;
+        }
+        animator.enabled = false;
+    }
+}
